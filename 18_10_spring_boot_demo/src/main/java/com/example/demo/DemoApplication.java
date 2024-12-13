8a9f23b50ae04579df9e4dd290bde7f35208cb36
Matthias Haeussler <maeddes@gmail.com>
2024-12-13 10:52:23 +0000

    Kubernetes yamls

--- 18_10_spring_boot_demo/src/main/java/com/example/demo/DemoApplication.java
+++ 18_10_spring_boot_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@GetMapping("/crash")
+	public String crash(){
+	
+		System.exit(1);
+		return "working code"; //statement cannot be reached
+	
+	}
+
+	@GetMapping("/hostname")
+	public String getHostname(){
+
+		try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            return "Hostname: " + inetAddress.getHostName();
+        } catch (UnknownHostException e) {
+            return "Error retrieving hostname: " + e.getMessage();
+        }
+
+	}
 
 	@GetMapping("/getHello")
 	public String sayHello(){
